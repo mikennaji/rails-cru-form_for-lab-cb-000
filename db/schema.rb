cb223044eb05d94d3ded7b7f71c5c95d6d1c0fad
Michael Nnaji <mnnaji3@uwo.ca>
2021-05-01 19:08:04 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210501160719) do
+ActiveRecord::Schema.define(version: 20210501182149) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
     t.string   "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
