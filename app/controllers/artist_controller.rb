1433dc07bcf400e1002e7db371c8d786501f1b74
Michael Nnaji <mnnaji3@uwo.ca>
2021-05-01 16:16:47 +0000

    Automatically backed up by Learn

--- app/controllers/artist_controller.rb
+++ app/controllers/artist_controller.rb
@@ -1,2 +1,39 @@
 class ArtistController < ApplicationController
+
+  def new 
+    @artist = Artist.new 
+  end 
+
+  def create 
+    @artist = Artist.new(artist_params(:name,:bio))
+    @artist.save
+    redirect_to artist_path(@artist)
+
+  end
+
+ def show 
+     @artist =  set_artist
+end 
+
+def edit 
+   @artist = set_artist
+end
+
+def update 
+   @artist = set_artist
+   @artist.update(artist_params(:name,:bio))
+   @artist.save
+   redirect_to artist_path(@artist)
+end 
+
+
+def set_artist
+   @artist = Artist.find(params[:id])
+end
+
+
+def artist_params(*args)
+  params.require(:arist).permit(*args)
+end
+
 end
