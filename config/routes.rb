110c73e602f438e255e73afb9a536ae8eff71507
Michael Nnaji <mnnaji3@uwo.ca>
2021-05-01 19:08:55 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  resources :artists, only: [:create,:new,:show,:edit,:update,:index]
+ resources :genres, only: [:create,:new,:show,:edit,:update,:index]
 end
